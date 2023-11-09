4bd026e067ef6ebc8419d8952f12018c1db9e45a
RebecaMFep <martinezfeliperebeca@gmail.com>
2023-11-09 11:21:27 +0100

    added home,game and results pages to vite

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,11 @@ export default defineConfig ({
                 main: resolve(__dirname,'src/index.html'),
                 about: resolve(__dirname, 'src/pages/about/index.html'),
                 contact: resolve(__dirname, 'src/pages/contact/index.html'),
+                home: resolve(__dirname, 'src/pages/home/index.html'),
+                game: resolve(__dirname, 'src/pages/game/index.html'),
+                results: (__dirname, 'src/pages/results/index.html')
             }
+
         }
     }
 })
