fa9c565aa1b27f3a10afc1ec6b54150d1d27921c
Borja <elbarmandev@gmail.com>
2023-11-10 14:25:26 +0100

    game logic finish (no styles)

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,11 +1,12 @@
-let players: string[] = [];
+let players: {name: string, balloon: string}[] = [];
 let eliminated: string[] = [];
 
 function addPlayer(name: string): void {
     if (players.length < 25) {
-        players.push(name);
-        alert(`Jugador ${name} ha sido añadido`);
+        let balloonNumber = Math.floor(Math.random() * 25) + 1; 
+        players.push({name: name, balloon: '../../assets/icons/balloon/balloon' + balloonNumber + '.svg'}); // guarda el nombre del jugador y el globo asignado
         updatePlayerList();
+        updatePlayerNameList();
     } else {
         alert("No se pueden añadir más de 25 jugadores")
     }
@@ -15,19 +16,26 @@ function resetGame(): void {
     players = [];
     eliminated = [];
     updatePlayerList();
+    updatePlayerNameList();
 }
 
 function play(): void {
     if (players.length < 2) {
         alert("Como mínimo deben de tener 2 jugadores")
     } else {
+        let sound = new Audio('../../assets/sounds/pinchazo.mp3'); 
+        sound.play(); 
+
         let eliminatedPlayer = players.splice(Math.floor(Math.random() * players.length), 1);
-        eliminated.push(eliminatedPlayer[0]);
-        alert(`el jugador ${eliminatedPlayer} ha sido eliminado`)
+        eliminated.push(eliminatedPlayer[0].name);
         updatePlayerList();
+        updatePlayerNameList();
+        updateEliminatedNameList();
         
         if (players.length === 1) {
-            alert(`¡El jugador ${players[0]} ha gando el cerdito!`);
+            let applauseSound = new Audio('../../assets/sounds/aplausos.mp3');
+            applauseSound.play();
+            alert(`¡El jugador ${players[0].name} ha gando el cerdito!`);
         }
     }
 }
@@ -41,7 +49,10 @@ function updatePlayerList(): void {
 
     for (let i = 0; i < players.length; i++) {
         let li = document.createElement('li');
-        li.textContent = players[i];
+        let img = document.createElement('img');
+        img.src = players[i].balloon;
+        img.alt = players[i].name;
+        li.appendChild(img);
         if (playerList) playerList.appendChild(li);
     }
 
@@ -51,3 +62,28 @@ function updatePlayerList(): void {
         if (eliminatedList) eliminatedList.appendChild(li);
     }
 }
+
+function updatePlayerNameList(): void {
+    let playerNameList = document.getElementById('playerNameList');
+
+    if (playerNameList) playerNameList.innerHTML = '';
+
+    for (let i = 0; i < players.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = players[i].name;
+        if (playerNameList) playerNameList.appendChild(li);
+    }
+}
+
+function updateEliminatedNameList(): void {
+    let eliminatedNameList = document.getElementById('eliminatedNameList');
+
+    if (eliminatedNameList) eliminatedNameList.innerHTML = '';
+
+    for (let i = 0; i < eliminated.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = eliminated[i];
+        if (eliminatedNameList) eliminatedNameList.appendChild(li);
+    }
+}
+
