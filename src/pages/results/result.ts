2310f974d7b2c93a9e82acc54c1effe7b5bfe1a9
Colunga-D <xkould@gmail.com>
2023-11-13 13:13:07 +0100

    results main end :s

---
+++ src/pages/results/result.ts
@@ -0,0 +1,15 @@
+// const ButtonReiniciar = document.getElementById('ButtonReiniciar');
+
+// ButtonReiniciar.addEventListener('click', () => {
+//     // Lógica para reiniciar los datos de todos los jugadores.
+//     reiniciarTodosLosDatosDeJugadores();
+
+//     // Actualizar la interfaz de usuario si es necesario.
+//     actualizarInterfazDeUsuario();
+// });
+
+// function reiniciarTodosLosDatosDeJugadores() {
+//     // Obtén la lista de jugadores (supongamos que está almacenada en una variable 'listaJugadores').
+//     for (const jugador of listaJugadores) {
+//         reiniciarDatosDelJugador(jugador);
+//     }
